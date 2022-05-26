bb35f0f3259159b335ad29f3ad85272edc9d6cbb
jacob knudsen <jhakeknudsen@protonmail.com>
2022-05-26 09:17:33 -0600

    1st commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 1.0.0 
+         created by Jake Knudsen 
+         05-25-22</h4>
+      <Link to='/'>Go Back</Link>
+    </div>
+  )
+}
+
+export default About
